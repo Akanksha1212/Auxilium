94c9acd806ab0ce62001e35bb1a1bd18fe13474b
Akanksha1212 <akankshasingh121297@gmail.com>
2020-10-04 18:40:52 +0530

    Help form

--- lib/screens/askhelp.dart
+++ lib/screens/askhelp.dart
@@ -8,61 +8,159 @@ class AskHelp extends StatefulWidget {
 }
 
 class AskHelpState extends State<AskHelp> {
+  double screenHeight;
+  var scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
+    screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-      backgroundColor: Color(0xff9ad7e9),
-      body: Column(
-        children: <Widget>[
-          SizedBox(
-            height: 40,
-          ),
-          Image(
-            image: AssetImage('assets/images/helpme.png'),
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 16, right: 16),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      "AskHelp",
-                      style: GoogleFonts.openSans(
-                          textStyle: TextStyle(
-                              color: Color(0xfff8f446),
-                              fontSize: 18,
-                              fontWeight: FontWeight.w600)),
+        key: scaffoldKey,
+        backgroundColor: Colors.lightBlue[50],
+        body: Container(
+            padding: const EdgeInsets.all(20.0),
+            color: Colors.lightBlue[50],
+            child: new SingleChildScrollView(
+              child: new ConstrainedBox(
+                constraints: new BoxConstraints(),
+                child: new Container(
+                  child: new Center(
+                      child: new Column(children: [
+                    new Padding(padding: EdgeInsets.only(top: 20.0)),
+                    Image.asset('assets/images/helpme.png'),
+                    new Padding(padding: EdgeInsets.only(top: 20.0)),
+                    new TextFormField(
+                      decoration: new InputDecoration(
+                        labelText: "Full Name",
+                        fillColor: Colors.white,
+                        border: new OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(25.0),
+                          borderSide: new BorderSide(),
+                        ),
+                      ),
+                      validator: (val) {
+                        if (val.length == 0) {
+                          return "Name can not be empty";
+                        } else {
+                          return null;
+                        }
+                      },
+                      keyboardType: TextInputType.text,
+                      style: new TextStyle(
+                        fontFamily: "Poppins",
+                      ),
                     ),
-                    Text(
-                      "AskHelp",
-                      style: GoogleFonts.openSans(
-                          textStyle: TextStyle(
-                              color: Color(0xfff8f446),
-                              fontSize: 18,
-                              fontWeight: FontWeight.w600)),
+                    new Padding(padding: EdgeInsets.only(top: 20.0)),
+                    new TextFormField(
+                      decoration: new InputDecoration(
+                        labelText: "Contact Number",
+                        fillColor: Colors.white,
+                        border: new OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(25.0),
+                          borderSide: new BorderSide(),
+                        ),
+                      ),
+                      validator: (val) {
+                        if (val.length == 0) {
+                          return "Contact number cannot be empty";
+                        } else {
+                          return null;
+                        }
+                      },
+                      keyboardType: TextInputType.number,
+                      style: new TextStyle(
+                        fontFamily: "Poppins",
+                      ),
                     ),
-                    Text(
-                      "AskHelp",
-                      style: GoogleFonts.openSans(
-                          textStyle: TextStyle(
-                              color: Color(0xfff8f446),
-                              fontSize: 18,
-                              fontWeight: FontWeight.w600)),
+                    new Padding(padding: EdgeInsets.only(top: 20.0)),
+                    new TextFormField(
+                      decoration: new InputDecoration(
+                        labelText: "Address",
+                        fillColor: Colors.white,
+                        border: new OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(25.0),
+                          borderSide: new BorderSide(),
+                        ),
+                      ),
+                      validator: (val) {
+                        if (val.length == 0) {
+                          return "Address cannot be empty";
+                        } else {
+                          return null;
+                        }
+                      },
+                      keyboardType: TextInputType.multiline,
+                      maxLines: 3,
+                      style: new TextStyle(
+                        fontFamily: "Poppins",
+                      ),
                     ),
-                  ],
+                    new Padding(padding: EdgeInsets.only(top: 20.0)),
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Container(
+                            height: 55,
+                            width: MediaQuery.of(context).size.width / 2,
+                            decoration: BoxDecoration(
+                              border:
+                                  Border.all(color: Colors.black45, width: 1),
+                              borderRadius: BorderRadius.circular(25.0),
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                DropDownButtonGender(),
+                              ],
+                            ),
+                          ),
+                        ]),
+                    new Padding(padding: EdgeInsets.only(top: 20.0)),
+                    RaisedButton(
+                      child: Text('Save'),
+                      color: Colors.blue[100],
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18),
+                          side: BorderSide(color: Colors.black)),
+                      onPressed: () {
+                        // save data
+                      },
+                    ),
+                  ])),
                 ),
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 40,
-          ),
-          // add form here
-        ],
-      ),
+              ),
+            )));
+  }
+}
+
+class DropDownButtonGender extends StatefulWidget {
+  DropDownButtonGender({Key key}) : super(key: key);
+
+  @override
+  _DropDownButtonGenderState createState() => _DropDownButtonGenderState();
+}
+
+class _DropDownButtonGenderState extends State<DropDownButtonGender> {
+  String dropdownValue = 'Male';
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: dropdownValue,
+      icon: Icon(Icons.arrow_downward),
+      iconSize: 24,
+      elevation: 16,
+      onChanged: (String newValue) {
+        setState(() {
+          dropdownValue = newValue;
+        });
+      },
+      items: <String>['Male', 'Female', 'Others']
+          .map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value, style: TextStyle(color: Colors.black54)),
+        );
+      }).toList(),
     );
   }
 }
